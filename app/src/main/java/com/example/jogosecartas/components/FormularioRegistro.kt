b58852f921fddffc2895ec142ec42cef97b1b3fe
pedro <pedrowilson.fon.port@gmail.com>
2025-03-15 19:09:48 -0300

    Criei um Formulario de registrro pra tela de registro deposi vry ta com pressa então merja isso

--- app/src/main/java/com/example/jogosecartas/components/FormularioRegistro.kt
+++ app/src/main/java/com/example/jogosecartas/components/FormularioRegistro.kt
@@ -1,2 +1,9 @@
 package com.example.jogosecartas.components
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun FormularioRegistro(navController: NavHostController){
+
+}
